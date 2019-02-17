dd072ed7ea45164687e5e54144fe30b597108fad
jsndc99 <40466093+jsndc99@users.noreply.github.com>
2019-02-17 12:04:51 +0530

    Commented

--- pdf_csv.py
+++ pdf_csv.py
@@ -2,13 +2,16 @@ import tabula
 
 # Read pdf into DataFrame
 df = tabula.read_pdf("Airports/EC-AD-2.1VABO-en-GB.pdf", output_format='csv', lattice=True, pages=4) #, area=(406, 24, 695, 589))
-clean_df = df.replace('\r',' ', regex=True)
+#Option to select pages automatically yet to be added
 
-# Read remote pdf into DataFrame
+clean_df = df.replace('\r',' ', regex=True) # This replaces \r in lattice mode to spaces
+
+# Read remote pdf into DataFrame, we can use this if doc is hosted online
 #df2 = tabula.read_pdf("https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")
 
 # convert PDF into CSV
-#tabula.convert_into("data.pdf", "output.csv", output_format="csv")
+#tabula.convert_into("data.pdf", "output.csv", output_format="csv") We don't use this though
+
 export_csv = df.to_csv (r'export_dataframe.csv', index = None, header=True)
 print(df)
 

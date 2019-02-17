363f4e4f50116256fbbb490ccaa737b3967c3358
jsndc99 <40466093+jsndc99@users.noreply.github.com>
2019-02-17 12:01:11 +0530

    Removed unnecessary newlines

--- pdf_csv.py
+++ pdf_csv.py
@@ -2,6 +2,7 @@ import tabula
 
 # Read pdf into DataFrame
 df = tabula.read_pdf("Airports/EC-AD-2.1VABO-en-GB.pdf", output_format='csv', lattice=True, pages=4) #, area=(406, 24, 695, 589))
+clean_df = df.replace('\r',' ', regex=True)
 
 # Read remote pdf into DataFrame
 #df2 = tabula.read_pdf("https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")
@@ -10,3 +11,7 @@ df = tabula.read_pdf("Airports/EC-AD-2.1VABO-en-GB.pdf", output_format='csv', la
 #tabula.convert_into("data.pdf", "output.csv", output_format="csv")
 export_csv = df.to_csv (r'export_dataframe.csv', index = None, header=True)
 print(df)
+
+export_csv = clean_df.to_csv (r'export_dataframe_clean.csv', index = None, header=True)
+print(clean_df)
+

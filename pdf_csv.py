cd828f7a0a34d07eab8982ca886ded0806a9787f
jsndc99 <40466093+jsndc99@users.noreply.github.com>
2019-02-18 14:03:42 +0530

    csv of column width generated

--- pdf_csv.py
+++ pdf_csv.py
@@ -1,9 +1,8 @@
-# Read remote pdf into DataFrame, we can use this if doc is hosted online
-#df2 = tabula.read_pdf("https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")
-
 # Thanks to user chezou on GitHub
 import tabula
 import PyPDF2
+import pandas
+import csv
 file="Airports/EC-AD-2.1VAAH-en-GB.pdf"
 
 pdf_reader = PyPDF2.PdfFileReader(file,strict=True)
@@ -18,15 +17,26 @@ for pg_no in range(0,pdf_reader.numPages):
 
 print('start='+str(start)+'\nend='+str(end))
 list_int=[]
-for num in range(start,end):
+for num in range(start,end+1):
     list_int.append(num)      
 
 # Read pdf into DataFrame
 list = tabula.read_pdf(file, output_format='csv', lattice=True, pages=list_int, multiple_tables=True) #, area=(406, 24, 695, 589))
 
+for df in list:
+    if len(df.columns) != 6:
+        list.remove(df)
+        continue
+
+#df_new = list[0]
+# df_new=pandas.DataFrame(columns=6)
+#for df in list:
+#    df_new.append(df) #, ignore_index=True)
+            
 i=0
 for df in list:
     clean_df = df.replace('\r',' ', regex=True) # This replaces \r in lattice mode to spaces
     export_csv = clean_df.to_csv (r'export_dataframe_clean_'+str(i)+'.csv', index = None, header=True) # This replaces \r in lattice mode to spaces
     print(clean_df)
     i+=1
+   

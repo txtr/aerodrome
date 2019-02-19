f245b25b8e5a6f3b37bea0b2532ac97be4c154a9
jsndc99 <40466093+jsndc99@users.noreply.github.com>
2019-02-19 22:12:51 +0530

    Cleant Branch, ready to merge
    Removed unnecessary bullshit, added requirements file, added ability to pass file as argv

--- CSV_KML/csv_kml.py
+++ CSV_KML/csv_kml.py
@@ -1,10 +1,15 @@
 # Written by Jason D'Costa
 # Pratik Chowdhury helped criticize my music choices including heavy metal
+# Include file as a command line argument
 
 import csv
-data = csv.reader(open('Bom.csv'), delimiter = ',')
+import sys  #sys.rgv[0] is name of script
+
+file=str(sys.argv[1])
+data = csv.reader(open(file), delimiter = ',')
 next(data)  #Skip the 1st header row.
-f = open('Bom_test.kml', 'w')   #Open the file to be written.
+output=file[:-4]    #Refer Slice Notation, everything except last 4 characters
+f = open(output+'.kml', 'w')   #Open the file to be written.
 
 f.write("<?xml version='1.0' encoding='UTF-8'?>\n") #Writing the kml file.
 f.write("<kml xmlns='http://earth.google.com/kml/2.1'>\n")
@@ -21,4 +26,4 @@ for row in data:
 f.write("</Document>\n")
 f.write("</kml>\n")
 f.close()
-print ("File Created.")
+print ("File Created with name: "+output+".kml")
